70ec973eea3456355ec7c59a80242fed717428bf
Collin Duncan <cgduncan7@gmail.com>
2020-05-24 22:17:45 +0200

    Added shim for @/types/GameMode

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -2,3 +2,8 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '@/types/GameMode' {
+  import GameMode from 'src/types/GameMode'
+  export default GameMode
+}
